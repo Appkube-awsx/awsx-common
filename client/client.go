24d5b0196ca414414d49b550b7cdd518b1f7d7d1
abdul-rajak <abdul.rajak@synectiks.com>
2023-05-18 16:06:48 +0530

    added aws-kms client

--- client/client.go
+++ client/client.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go/service/costexplorer"
+	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
@@ -20,12 +21,14 @@ const (
 	LAMBDA_CLIENT  = "lambda"
 	COST_EXPLORER  = "costExplorer"
 	CLOUDWATCH_LOG = "cloudWatchLog"
+	KMS_CLIENT     = "kms"
 )
 
 var clients = map[string]func(*session.Session) interface{}{
 	LAMBDA_CLIENT:  func(session *session.Session) interface{} { return lambda.New(session) },
 	COST_EXPLORER:  func(session *session.Session) interface{} { return costexplorer.New(session) },
 	CLOUDWATCH_LOG: func(session *session.Session) interface{} { return cloudwatchlogs.New(session) },
+	KMS_CLIENT:     func(session *session.Session) interface{} { return kms.New(session) },
 }
 
 // GetClient is returns aws clients
