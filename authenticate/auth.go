ff6b4aa17e18f17c81b7894dd9b025e15df2b0ba
ManojSynectiks <manoj.sharma@synectiks.com>
2023-11-27 15:01:01 +0530

    user credential by cloud-element-id api call

--- authenticate/auth.go
+++ authenticate/auth.go
@@ -6,9 +6,40 @@ import (
 	"github.com/Appkube-awsx/awsx-common/vault"
 	"github.com/spf13/cobra"
 	"log"
+	"net/http"
 )
 
-func AuthenticateData(vaultUrl string, vaultToken string, accountNo string, region string, acKey string, secKey string, crossAccountRoleArn string, externalId string) (bool, *client.Auth, error) {
+func AuthenticateData(cloudElementId string, cloudElementApiUrl string, vaultUrl string, vaultToken string, accountNo string, region string, acKey string, secKey string, crossAccountRoleArn string, externalId string) (bool, *client.Auth, error) {
+	if cloudElementId != "" {
+		log.Println("cloud-element-id provided. getting user credentials. cloud-element-id: " + cloudElementId)
+		if cloudElementApiUrl == "" {
+			log.Println("cloud-element api url missing")
+			return false, nil, fmt.Errorf("cloud-element api url missing")
+		}
+		apiResp, statusCode, err := vault.GetUserCredential(cloudElementId, cloudElementApiUrl)
+		if err != nil {
+			log.Println("call to cloud-element api failed. \n", err)
+			return false, nil, err
+		}
+		if statusCode != http.StatusOK {
+			log.Println("error in calling cloud-element api. status code: "+string(statusCode)+" \n", err)
+			return false, nil, err
+		}
+		clientAuth := client.Auth{
+			CrossAccountRoleArn: apiResp.Data.CrossAccountRoleArn,
+			AccessKey:           apiResp.Data.AccessKey,
+			SecretKey:           apiResp.Data.SecretKey,
+			ExternalId:          apiResp.Data.ExternalId,
+		}
+		if region != "" {
+			clientAuth.Region = region
+		} else {
+			log.Println("region not provided. default region will be used")
+			clientAuth.Region = apiResp.Data.Region
+		}
+		return true, &clientAuth, nil
+	}
+
 	if vaultUrl != "" {
 		log.Println("vault url provided. getting user credentials from vault")
 		if vaultToken == "" {
@@ -76,6 +107,8 @@ func AuthenticateData(vaultUrl string, vaultToken string, accountNo string, regi
 }
 
 func CommandAuth(cmd *cobra.Command) (bool, *client.Auth, error) {
+	cloudElementId, _ := cmd.PersistentFlags().GetString("cloudElementId")
+	cloudElementApiUrl, _ := cmd.PersistentFlags().GetString("cloudElementApiUrl")
 	vaultUrl, _ := cmd.PersistentFlags().GetString("vaultUrl")
 	vaultToken, _ := cmd.PersistentFlags().GetString("vaultToken")
 	accountNo, _ := cmd.PersistentFlags().GetString("accountId")
@@ -84,11 +117,13 @@ func CommandAuth(cmd *cobra.Command) (bool, *client.Auth, error) {
 	secKey, _ := cmd.PersistentFlags().GetString("secretKey")
 	crossAccountRoleArn, _ := cmd.PersistentFlags().GetString("crossAccountRoleArn")
 	externalId, _ := cmd.PersistentFlags().GetString("externalId")
-	authFlag, clientAuth, err := AuthenticateData(vaultUrl, vaultToken, accountNo, region, acKey, secKey, crossAccountRoleArn, externalId)
+	authFlag, clientAuth, err := AuthenticateData(cloudElementId, cloudElementApiUrl, vaultUrl, vaultToken, accountNo, region, acKey, secKey, crossAccountRoleArn, externalId)
 	return authFlag, clientAuth, err
 }
 
 func SubCommandAuth(cmd *cobra.Command) (bool, *client.Auth, error) {
+	cloudElementId, _ := cmd.Parent().PersistentFlags().GetString("cloudElementId")
+	cloudElementApiUrl, _ := cmd.Parent().PersistentFlags().GetString("cloudElementApiUrl")
 	vaultUrl, _ := cmd.Parent().PersistentFlags().GetString("vaultUrl")
 	vaultToken, _ := cmd.Parent().PersistentFlags().GetString("vaultToken")
 	accountNo, _ := cmd.Parent().PersistentFlags().GetString("accountId")
@@ -97,6 +132,6 @@ func SubCommandAuth(cmd *cobra.Command) (bool, *client.Auth, error) {
 	secKey, _ := cmd.Parent().PersistentFlags().GetString("secretKey")
 	crossAccountRoleArn, _ := cmd.Parent().PersistentFlags().GetString("crossAccountRoleArn")
 	externalId, _ := cmd.Parent().PersistentFlags().GetString("externalId")
-	authFlag, clientAuth, err := AuthenticateData(vaultUrl, vaultToken, accountNo, region, acKey, secKey, crossAccountRoleArn, externalId)
+	authFlag, clientAuth, err := AuthenticateData(cloudElementId, cloudElementApiUrl, vaultUrl, vaultToken, accountNo, region, acKey, secKey, crossAccountRoleArn, externalId)
 	return authFlag, clientAuth, err
 }
--- vault/vault-client.go
+++ vault/vault-client.go
@@ -1,7 +1,10 @@
 package vault
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -21,6 +24,13 @@ type VaultResponse struct {
 	Renewable     string        `json:"renewable,omitempty"`
 	LeaseDuration int64         `json:"lease_duration,omitempty"`
 	Data          AwsCredential `json:"data,omitempty"`
+}
+
+type ApiResponse struct {
+	Status     string        `json:"status,omitempty"`
+	Message    string        `json:"message,omitempty"`
+	StatusCode string        `json:"statusCode,omitempty"`
+	Data       AwsCredential `json:"data,omitempty"`
 }
 
 func GetAccountDetails(vaultUrl string, vaultToken string, accountNo string) (*VaultResponse, error) {
@@ -49,4 +59,55 @@ func GetAccountDetails(vaultUrl string, vaultToken string, accountNo string) (*V
 	json.Unmarshal(bodyBytes, &responseObject)
 	return &responseObject, nil
 
+}
+
+func GetUserCredential(cloudElementId string, apiUrl string) (apiResponse *ApiResponse, statusCode int, err error) {
+	log.Println("Getting user's cloud credentials by cloud-element-id")
+	url := apiUrl + "?cloudElementId=" + cloudElementId
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err.Error())
+		return nil, http.StatusInternalServerError, err
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	res, err := client.Do(req)
+	if res != nil {
+		defer res.Body.Close()
+	}
+	if err != nil && res != nil {
+		fmt.Println("error in getting response from server", "url", url, "method", req.Method, "error", err.Error(), "status code", res.StatusCode)
+		return nil, res.StatusCode, fmt.Errorf("error in getting response from server. api url %s", url)
+	}
+	if err != nil && res == nil {
+		fmt.Println("error getting response from server. no response received", "url", url, "error", err.Error())
+		return nil, http.StatusInternalServerError, fmt.Errorf("error getting response from server. no response received. api url %s. no response received. Error: %s", url, err.Error())
+	}
+	if err == nil && res == nil {
+		fmt.Println("invalid response from server and also no error", "url", url, "method", req.Method)
+		return nil, http.StatusInternalServerError, fmt.Errorf("invalid response from server and also no error. api url %s", url)
+	}
+	if res.StatusCode >= http.StatusBadRequest {
+		return nil, res.StatusCode, fmt.Errorf(res.Status)
+	}
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("error reading response body", "url", url, "error", err.Error())
+		return nil, res.StatusCode, err
+	}
+	bodyBytes = removeBOMContent(bodyBytes)
+	var responseObject ApiResponse
+	jsonMarshalError := json.Unmarshal(bodyBytes, &responseObject)
+	if jsonMarshalError != nil {
+		fmt.Println("json marshal error", "url", url, "error", jsonMarshalError.Error())
+		return nil, http.StatusInternalServerError, jsonMarshalError
+	}
+	return &responseObject, http.StatusOK, nil
+}
+
+func removeBOMContent(input []byte) []byte {
+	return bytes.TrimPrefix(input, []byte("\xef\xbb\xbf"))
 }
